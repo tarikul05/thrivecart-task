370e91c9ba604bb69b033da8a916fedbce88ef8a
Tarikul Islam <tarikul.isram@reivo.co.jp>
2024-08-20 20:24:20 +0600

    add initializer for basket class

--- src/Basket.php
+++ src/Basket.php
@@ -1,3 +1,13 @@
 <?php
-phpinfo();
-?>
+class Basket {
+    private $products;
+    private $deliveryRules;
+    private $offers;
+
+    public function __construct($products, $deliveryRules, $offers) {
+        $this->products = $products;
+        $this->deliveryRules = $deliveryRules;
+        $this->offers = $offers;
+    }
+
+}
